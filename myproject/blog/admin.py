a01333bcee04b8a83d8ee09844aaa7f8f1c55bbf
a.adli <amirhossein.adli@gmail.com>
2022-05-05 10:13:04 +0430

    added Category model into models.py added Category to admin.py

--- myproject/blog/admin.py
+++ myproject/blog/admin.py
@@ -1,13 +1,28 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Category
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display: tuple = ("position", "title", "slug", "status",)
+    list_filter: tuple = (["status"])
+    search_fields: tuple = ("title", "slug",)
+    prepopulated_fields: dict = {"slug": ("title",)}
+
+
+# Register your models here.
+admin.site.register(Category, CategoryAdmin)
 
 
 class ArticleAdmin(admin.ModelAdmin):
-    list_display: tuple = ("title", "slug", "description", "publish_datetime", "status",)
+    list_display: tuple = ("title", "slug", "description", "publish_datetime", "status", "_category_to_str",)
     list_filter: tuple = ("publish_datetime", "status",)
     search_fields: tuple = ("title", "description",)
     prepopulated_fields: dict = {"slug": ("title",)}
     ordering: list[str] = ["status", "-publish_datetime"]
+
+    def _category_to_str(self, obj: Article) -> str:
+        return "، ".join([category.title for category in (obj.category.all())])
+    _category_to_str.short_description: str = "دسته بندی"
 
 
 # Register your models here.
--- myproject/blog/models.py
+++ myproject/blog/models.py
@@ -2,6 +2,20 @@ from datetime import datetime
 
 from django.db import models
 
+class Category(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=100, unique=True, verbose_name="آدرس دسته بندی")
+    status = models.BooleanField(default=True, verbose_name="آیا نمایش داده شود؟")
+    position = models.IntegerField(verbose_name="پوزیشن")
+
+    class Meta:
+        verbose_name: str = "دسته بندی"
+        verbose_name_plural: str = "دسته بندی ها"
+        ordering: list[str] = ['position']
+
+    def __str__(self):
+        return self.title
+
 
 # Create your models here.
 class Article(models.Model):
@@ -11,6 +25,7 @@ class Article(models.Model):
     )
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=100, unique=True)
+    category = models.ManyToManyField(Category, verbose_name="دسته بندی")
     description = models.TextField()
     thumbnail = models.ImageField(upload_to="images")
     publish_datetime = models.DateTimeField(default=datetime.utcnow())
